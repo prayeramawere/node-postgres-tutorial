ce59aa6f0037cfc6d15104948ee4c0f68d9a3c86
Prayer Mawere <prayermawererl@gmail.com>
2026-01-20 11:03:43 +0200

    packages update

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import { Client } from "pg";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
 const con = new Client({
   host: "localhost",
   user: "postgres",
@@ -8,3 +13,29 @@ const con = new Client({
 });
 
 await con.connect().then(() => console.log("connected"));
+
+app.post("/post", async (req, res) => {
+  const { name, id } = req.body;
+
+  const insert_query = `INSERT INTO demotable (name,id) VALUES ('${name}', '${id}')`;
+  try {
+    const response = await con.query(insert_query);
+    res.json({ success: true, data: response });
+  } catch (error) {
+    res.json({ success: false, msg: error });
+  }
+});
+
+app.get("/fetch-data", (req, res) => {
+  const fetch_query = `SELECT * FROM demotable`;
+
+  con.query(fetch_query, (err, result) => {
+    err
+      ? res.send({ success: false, msg: err })
+      : res.send({ success: true, data: result.rows });
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`app running live at: http://localhost:4000`);
+});
